308be43a8999b0b20a15f1555ce99388a16f8631
Minik Gaarde Lambrecht <mini0040@edu.sde.dk>
2018-06-14 14:34:47 +0200

    Update - Checkout tab created

--- IsButik/MainWindow.xaml.cs
+++ IsButik/MainWindow.xaml.cs
@@ -6,6 +6,8 @@ using System.Reflection;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using System;
+using System.Windows.Media;
+using System.Windows.Input;
 
 namespace IsButik
 {
@@ -26,6 +28,7 @@ namespace IsButik
 
             // Initialize UpdateValues Method to update data to program
             UpdateValues();
+            // Initialize CreateDirectories Method to create needed directories if not created already 
             CreateDirectories();
         }
 
@@ -51,8 +54,11 @@ namespace IsButik
                 string line;
                 while ((line = reader.ReadLine()) != null)
                 {
+                    // If the current line the streamreader is reading is not null then create a string array named lines and split teh string for each "," in the current string
                     string[] lines = Regex.Split(line, ",");
+                    // Add the first string in the string array to the string list
                     Values.containers.Add(lines[0]);
+                    // Add the second string in the string array to the other string list
                     containerPrices.Add(lines[1]);
                 }
             }
@@ -77,6 +83,7 @@ namespace IsButik
                 string line;
                 while ((line = reader.ReadLine()) != null)
                 {
+                    // If the current line the streamreader is reading is not null then add the current line to the string list
                     Values.scoops_Cup.Add(line);
                 }
             }
@@ -99,6 +106,7 @@ namespace IsButik
                 string line;
                 while ((line = reader.ReadLine()) != null)
                 {
+                    // If the current line the streamreader is reading is not null then add the current line to the string list
                     Values.scoops_SmallCone.Add(line);
                 }
             }
@@ -121,6 +129,7 @@ namespace IsButik
                 string line;
                 while ((line = reader.ReadLine()) != null)
                 {
+                    // If the current line the streamreader is reading is not null then add the current line to the string list
                     Values.scoops_MediumCone.Add(line);
                 }
             }
@@ -143,6 +152,7 @@ namespace IsButik
                 string line;
                 while ((line = reader.ReadLine()) != null)
                 {
+                    // If the current line the streamreader is reading is not null then add the current line to the string list
                     Values.scoops_BigCone.Add(line);
                 }
             }
@@ -172,6 +182,7 @@ namespace IsButik
                 string line;
                 while ((line = reader.ReadLine()) != null)
                 {
+                    // If the current line the streamreader is reading is not null then add the current line to the string list
                     Values.sprinkles.Add(line);
                 }
             }
@@ -186,8 +197,11 @@ namespace IsButik
 
         private void CreateDirectories()
         {
+            // Path to appdata folder
             string appdata = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            // Path to create root directory with
             string rootDirectory = Path.Combine(appdata, "IceCreamShop");
+            // Path to create subdirectory with
             string subDir = Path.Combine(rootDirectory, "Settings");
 
             // If directory does not exist, create it. 
@@ -196,6 +210,7 @@ namespace IsButik
                 Directory.CreateDirectory(rootDirectory);
             }
 
+            // If directory does not exist, create it. 
             if (!Directory.Exists(subDir))
             {
                 Directory.CreateDirectory(subDir);
@@ -278,6 +293,7 @@ namespace IsButik
 
         private void RB_SN0_Checked(object sender, RoutedEventArgs e)
         {
+            // If checked this will make all radio buttons below invisible, change the totalSprinklePrice int and the txt_Sprinkles text
             CB_Sprinkles_1.Visibility = Visibility.Hidden;
             CB_Sprinkles_2.Visibility = Visibility.Hidden;
             CB_Sprinkles_3.Visibility = Visibility.Hidden;
@@ -288,6 +304,7 @@ namespace IsButik
 
         private void RB_SN1_Checked(object sender, RoutedEventArgs e)
         {
+            // If checked this will make the two last radio buttons below invisible, change the totalSprinklePrice int and the txt_Sprinkles text
             CB_Sprinkles_1.Visibility = Visibility.Visible;
             CB_Sprinkles_2.Visibility = Visibility.Hidden;
             CB_Sprinkles_3.Visibility = Visibility.Hidden;
@@ -298,6 +315,7 @@ namespace IsButik
 
         private void RB_SN2_Checked(object sender, RoutedEventArgs e)
         {
+            // If checked this will the last radio button below invisible, change the totalSprinklePrice int and the txt_Sprinkles text
             CB_Sprinkles_1.Visibility = Visibility.Visible;
             CB_Sprinkles_2.Visibility = Visibility.Visible;
             CB_Sprinkles_3.Visibility = Visibility.Hidden;
@@ -308,6 +326,7 @@ namespace IsButik
 
         private void RB_SN3_Checked(object sender, RoutedEventArgs e)
         {
+            // If checked this will make all radio buttons below visible, change the totalSprinklePrice int and the txt_Sprinkles text
             CB_Sprinkles_1.Visibility = Visibility.Visible;
             CB_Sprinkles_2.Visibility = Visibility.Visible;
             CB_Sprinkles_3.Visibility = Visibility.Visible;
@@ -316,14 +335,315 @@ namespace IsButik
             txt_Sprinkles.Content = "Sprinkle(s): " + totalSprinklePrice + "$";
         }
 
-        #endregion
-
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Btn_Exit_Click(object sender, RoutedEventArgs e)
         {
+            // Opens a messagebox to make sure you want to quit, if ok is cliked you will exit the application
             if (MessageBox.Show("Are you sure you want to exit?", "Exit", MessageBoxButton.OKCancel) == MessageBoxResult.OK)
             {
                 Application.Current.Shutdown();
             }
         }
+
+        private void Btn_Settings_Click(object sender, RoutedEventArgs e)
+        {
+            // Opens the settings window and closes this window
+            Settings settings = new Settings();
+            settings.Show();
+            this.Close();
+        }
+
+        private void Btn_PlaceOrder_Click(object sender, RoutedEventArgs e)
+        {
+            // Changes from current tab to the checkout tab
+            TabCtrl.SelectedIndex = 2;
+        }
+
+        private void txt_FullName_GotFocus(object sender, RoutedEventArgs e)
+        {
+            // If the textbox is not null do nothing
+            if (!string.IsNullOrEmpty(txt_FullName.Text))
+            {
+
+            }
+
+            // If the textbox has default content, clear it
+            if (txt_FullName.Text == "Full Name")
+            {
+                txt_FullName.Clear();
+            }
+
+            // Change the borderbrush color if it's not already black
+            txt_Exp_Year.BorderBrush = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FF000000"));
+        }
+
+        private void txt_FullName_LostFocus(object sender, RoutedEventArgs e)
+        {
+            // If the textbox is not null go on, else change content back to defualt
+            if (!string.IsNullOrEmpty(txt_FullName.Text))
+            {
+                // If text is greater than 2 then go on, else if text equals 0 then change the content back to default. If none of the options make the borderbrush red
+                if (txt_FullName.Text.Length > 49)
+                {
+                    // If the borderbrush equals red then change it back to black
+                    if (txt_FullName.BorderBrush == Brushes.Red)
+                    {
+                        txt_FullName.BorderBrush = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FF000000"));
+                    }
+                    else
+                    {
+
+                    }
+                }
+                else if (txt_FullName.Text.Length == 0)
+                {
+                    txt_FullName.Text = "Full Name";
+                }
+            }
+            else
+            {
+                txt_FullName.Text = "Full Name";
+            }
+        }
+
+        private void txt_FullName_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            // Allows only alphabetic input, if no letters are pressed no input will come
+            e.Handled = Regex.IsMatch(e.Text, @"^[a-zA-Z]+$");
+        }
+
+        private void txt_CardNo_GotFocus(object sender, RoutedEventArgs e)
+        {
+            // If the textbox is not null do nothing
+            if (!string.IsNullOrEmpty(txt_CardNo.Text))
+            {
+
+            }
+
+            // If the textbox has default content, clear it
+            if (txt_CardNo.Text == "**** **** **** ****")
+            {
+                txt_CardNo.Clear();
+            }
+
+            // Change the borderbrush color if it's not already black
+            txt_CardNo.BorderBrush = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FF000000"));
+        }
+
+        private void txt_CardNo_LostFocus(object sender, RoutedEventArgs e)
+        {
+            // If the textbox is not null go on, else change content back to defualt
+            if (!string.IsNullOrEmpty(txt_CardNo.Text))
+            {
+                // If text is longer or equals to 16 then go on, else if text equals 0 then change the content back to default. If none of the options make the borderbrush red
+                if (txt_CardNo.Text.Length >= 16)
+                {
+                    txt_CardNo.Text = txt_CardNo.Text.Insert(4, " ");
+                    txt_CardNo.Text = txt_CardNo.Text.Insert(9, " ");
+                    txt_CardNo.Text = txt_CardNo.Text.Insert(14, " ");
+
+                    // If the borderbrush equals red then change it back to black
+                    if (txt_CardNo.BorderBrush == Brushes.Red)
+                    {
+                        txt_CardNo.BorderBrush = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FF000000"));
+                    }
+                    else
+                    {
+
+                    }
+                }
+                else if (txt_CardNo.Text.Length == 0)
+                {
+                    txt_CardNo.Text = "**** **** **** ****";
+                }
+                else
+                {
+                    txt_CardNo.BorderBrush = Brushes.Red;
+                }
+            }
+            else
+            {
+                txt_CardNo.Text = "**** **** **** ****";
+            }
+        }
+
+        private void txt_CardNo_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            // Allows only numeric input, if nothing numeric is pressed no input will come
+            e.Handled = Regex.IsMatch(e.Text, "[^0-9]+");
+        }
+
+        private void txt_Exp_Month_GotFocus(object sender, RoutedEventArgs e)
+        {
+            // If the textbox is not null do nothing
+            if (!string.IsNullOrEmpty(txt_Exp_Month.Text))
+            {
+
+            }
+
+            // If the textbox has default content, clear it
+            if (txt_Exp_Month.Text == "MM")
+            {
+                txt_Exp_Month.Clear();
+            }
+
+            // Change the borderbrush color if it's not already black
+            txt_Exp_Month.BorderBrush = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FF000000"));
+        }
+
+        private void txt_Exp_Month_LostFocus(object sender, RoutedEventArgs e)
+        {
+            // If the textbox is not null go on, else change content back to defualt
+            if (!string.IsNullOrEmpty(txt_Exp_Month.Text))
+            {
+                // If text is greater than 1 then go on, else if text equals 0 then change the content back to default. If none of the options make the borderbrush red
+                if (txt_Exp_Month.Text.Length > 1)
+                {
+                    // If the borderbrush equals red then change it back to black
+                    if (txt_Exp_Month.BorderBrush == Brushes.Red)
+                    {
+                        txt_Exp_Month.BorderBrush = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FF000000"));
+                    }
+                    else
+                    {
+
+                    }
+                }
+                else if (txt_Exp_Month.Text.Length == 0)
+                {
+                    txt_Exp_Month.Text = "MM";
+                }
+                else
+                {
+                    txt_Exp_Month.BorderBrush = Brushes.Red;
+                }
+            }
+            else
+            {
+                txt_Exp_Month.Text = "MM";
+            }
+        }
+
+        private void txt_Exp_Month_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            // Allows only numeric input, if nothing numeric is pressed no input will come
+            e.Handled = Regex.IsMatch(e.Text, "[^0-9]+");
+        }
+
+        private void txt_Exp_Year_GotFocus(object sender, RoutedEventArgs e)
+        {
+            // If the textbox is not null do nothing
+            if (!string.IsNullOrEmpty(txt_Exp_Year.Text))
+            {
+
+            }
+
+            // If the textbox has default content, clear it
+            if (txt_Exp_Year.Text == "YY")
+            {
+                txt_Exp_Year.Clear();
+            }
+
+            // Change the borderbrush color if it's not already black
+            txt_Exp_Year.BorderBrush = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FF000000"));
+        }
+
+        private void txt_Exp_Year_LostFocus(object sender, RoutedEventArgs e)
+        {
+            // If the textbox is not null go on, else change content back to defualt
+            if (!string.IsNullOrEmpty(txt_Exp_Year.Text))
+            {
+                // If text is greater than 1 then go on, else if text equals 0 then change the content back to default. If none of the options make the borderbrush red
+                if (txt_Exp_Year.Text.Length > 1)
+                {
+                    // If the borderbrush equals red then change it back to black
+                    if (txt_Exp_Year.BorderBrush == Brushes.Red)
+                    {
+                        txt_Exp_Year.BorderBrush = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FF000000"));
+                    }
+                    else
+                    {
+
+                    }
+                }
+                else if (txt_Exp_Year.Text.Length == 0)
+                {
+                    txt_Exp_Year.Text = "YY";
+                }
+                else
+                {
+                    txt_Exp_Year.BorderBrush = Brushes.Red;
+                }
+            }
+            else
+            {
+                txt_Exp_Year.Text = "YY";
+            }
+        }
+
+        private void txt_Exp_Year_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            // Allows only numeric input, if nothing numeric is pressed no input will come
+            e.Handled = Regex.IsMatch(e.Text, "[^0-9]+");
+        }
+
+        private void txt_CVC_GotFocus(object sender, RoutedEventArgs e)
+        {
+            // If the textbox is not null do nothing
+            if (!string.IsNullOrEmpty(txt_CVC.Text))
+            {
+
+            }
+
+            // If the textbox has default content, clear it
+            if (txt_CVC.Text == "CVC")
+            {
+                txt_CVC.Clear();
+            }
+
+            // Change the borderbrush color if it's not already black
+            txt_Exp_Year.BorderBrush = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FF000000"));
+        }
+
+        private void txt_CVC_LostFocus(object sender, RoutedEventArgs e)
+        {
+            // If the textbox is not null go on, else change content back to defualt
+            if (!string.IsNullOrEmpty(txt_CVC.Text))
+            {
+                // If text is greater than 2 then go on, else if text equals 0 then change the content back to default. If none of the options make the borderbrush red
+                if (txt_CVC.Text.Length > 2)
+                {
+                    // If the borderbrush equals red then change it back to black
+                    if (txt_CVC.BorderBrush == Brushes.Red)
+                    {
+                        txt_CVC.BorderBrush = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FF000000"));
+                    }
+                    else
+                    {
+
+                    }
+                }
+                else if (txt_CVC.Text.Length == 0)
+                {
+                    txt_CVC.Text = "CVC";
+                }
+                else
+                {
+                    txt_CVC.BorderBrush = Brushes.Red;
+                }
+            }
+            else
+            {
+                txt_CVC.Text = "CVC";
+            }
+        }
+
+        private void txt_CVC_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            // Allows only numeric input, if nothing numeric is pressed no input will come
+            e.Handled = Regex.IsMatch(e.Text, "[^0-9]+");
+        }
+
+        #endregion
+
     }
 }
--- IsButik/Settings.xaml.cs
+++ IsButik/Settings.xaml.cs
@@ -14,14 +14,63 @@ using System.Windows.Shapes;
 
 namespace IsButik
 {
-    /// <summary>
-    /// Interaction logic for Settings.xaml
-    /// </summary>
     public partial class Settings : Window
     {
         public Settings()
         {
             InitializeComponent();
+
+            // Initialize GetsavedSettings Method to load every saved setting
+            GetSavedSettings();
         }
+
+        #region Events
+
+        private void RB_SaveLocally_Checked(object sender, RoutedEventArgs e)
+        {
+            // If checked change settings to be saved
+            Properties.Settings.Default.saveLocally = true;
+            Properties.Settings.Default.saveInDB = false;
+        }
+
+        private void RB_SaveInDB_Checked(object sender, RoutedEventArgs e)
+        {
+            // If checked change settings to be saved
+            Properties.Settings.Default.saveLocally = false;
+            Properties.Settings.Default.saveInDB = true;
+        }
+
+        private void Btn_Save_Click(object sender, RoutedEventArgs e)
+        {
+            // Saves the settings
+            Properties.Settings.Default.Save();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Closes current window and opens the mainwindows again
+            MainWindow mw = new MainWindow();
+            mw.Show();
+            this.Close();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            // If program unexpectedly shuts down or is closed by the x button, then save the settings before closing
+            Properties.Settings.Default.Save();
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void GetSavedSettings()
+        {
+            RB_SaveLocally.IsChecked = Properties.Settings.Default.saveLocally;
+            RB_SaveInDB.IsChecked = Properties.Settings.Default.saveInDB;
+        }
+
+        #endregion
+
     }
 }
